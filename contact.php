6d4c310a89228f65608807793da03cd84742834e
Slavic2004 <malaianonim01@gmail.com>
2025-05-21 10:29:31 +0300

    lab 4 add php

--- contact.php
+++ contact.php
@@ -15,10 +15,10 @@
 
     <!-- Bara de navigare adăugată -->
     <nav>
-        <a href="index.html">Acasă</a>
-        <a href="noutati.html">Noutăți</a>
-        <a href="meciuri.html">Meciuri</a>
-        <a href="contact.html">Contact</a>
+        <a href="index.php">Acasă</a>
+        <a href="noutati.php">Noutăți</a>
+        <a href="meciuri.php">Meciuri</a>
+        <a href="contact.php">Contact</a>
     </nav>
 
     <div class="container">
@@ -39,7 +39,7 @@
         </div>
 
         <h2>Formular de Contact</h2>
-        <form action="#" method="POST">
+        <form action="save_contact.php" method="POST">
             <label for="name">Nume:</label><br>
             <input type="text" id="name" name="name" required><br><br>
 
